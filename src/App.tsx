be81e10195f1f4e4542ec83e7ee0db46deb2d9da
naopeke <naopeke@protonmail.com>
2024-11-29 15:37:55 +0100

    setting

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    <div>Landing</div>
       {/* <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
