05efe6bedc3e6bc3ebb8f956c86c49bcfeccfa01
naopeke <naopeke@protonmail.com>
2024-12-01 09:43:54 +0100

    Heading

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,14 +1,15 @@
 import { ReactNode } from "react";
 
+//Headingコンポーネントに渡されるpropsの型を定義
 interface HeadingProps extends React.HTMLAttributes<HTMLHeadingElement> {
     level?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
     title?: string;
     className?: string;
-    children: ReactNode; 
+    children: ReactNode; //コンポーネントの子要素。見出しタグ内に表示する内容
 }
 
 function Heading(props: HeadingProps){
-    const {level, title, className, children, ...restProps} = props;
+    const {level, title, className, children, ...restProps} = props; //{...restProps}は、HTMLAttributes<HTMLHeadingElement>で受け取ったその他の属性（id、styleなど）を展開して渡します。
     const validHeadingTags = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6']
     // levelがvalidHeadingTagsに含まれていればそのタグを使用、含まれていなければデフォルトで'h1'タグ
     const HeadingTag = level && validHeadingTags.includes(level) ? level : 'h1';
@@ -21,11 +22,12 @@ function Heading(props: HeadingProps){
         h6: 'text-base font-bold my-2',
     }
 
+    //もしchildrenが渡されていればそれを表示し、渡されていなければtitleが表示
     return (
         <>
-        <HeadingTag className={`${headingClasses[HeadingTag]} ${className}`}{...restProps}>
-            {children || title}
-        </HeadingTag>
+            <HeadingTag className={`${headingClasses[HeadingTag]} ${className}`}{...restProps}>
+                {children || title}
+            </HeadingTag>
         </>
     )
 }
